a78211fba91af04270183e058f3b93cca86fc489
maxidea1024@gmail.com <maxidea1024@gmail.com>
2024-12-05 18:24:21 +0900

    adjustments

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { start } from './lib/server-impl';
 
 try {
   start();
-} catch (error) {
+} catch (error: unknown) {
   // eslint-disable-next-line no-console
   console.error(error);
   process.exit();
