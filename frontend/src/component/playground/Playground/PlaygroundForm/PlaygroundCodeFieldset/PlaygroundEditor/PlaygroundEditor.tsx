d020927ffb058608115ec11ace208bb48f10a275
jhseo <maxidea1024@gmail.com>
2025-03-17 20:08:25 +0900

    Update PlaygroundEditor.tsx

--- frontend/src/component/playground/Playground/PlaygroundForm/PlaygroundCodeFieldset/PlaygroundEditor/PlaygroundEditor.tsx
+++ frontend/src/component/playground/Playground/PlaygroundForm/PlaygroundCodeFieldset/PlaygroundEditor/PlaygroundEditor.tsx
@@ -3,7 +3,6 @@ import { useContext } from 'react';
 import { json } from '@codemirror/lang-json';
 import { type Dispatch, type SetStateAction, useCallback } from 'react';
 import { styled, useTheme, Box } from '@mui/material';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { duotoneDark, duotoneLight } from '@uiw/codemirror-theme-duotone';
 import Check from '@mui/icons-material/Check';
 import ErrorIcon from '@mui/icons-material/Error';
@@ -96,21 +95,19 @@ export const PlaygroundEditor = ({
     <Box sx={{ width: '100%' }}>
       <StyledEditorHeader>
         JSON
-        <ConditionallyRender
-          condition={Boolean(error)}
-          show={
-            <Box
-              sx={(theme) => ({
-                display: 'flex',
-                alignItems: 'center',
-              })}
-            >
-              <StyledErrorSpan>{error}</StyledErrorSpan>
-              <EditorStatusError />
-            </Box>
-          }
-          elseShow={<EditorStatusOk />}
-        />
+        {error ? (
+          <Box
+            sx={(theme) => ({
+              display: 'flex',
+              alignItems: 'center',
+            })}
+          >
+            <StyledErrorSpan>{error}</StyledErrorSpan>
+            <EditorStatusError />
+          </Box>
+        ) : (
+          <EditorStatusOk />
+        )}
       </StyledEditorHeader>
       <CodeMirror
         value={context}
